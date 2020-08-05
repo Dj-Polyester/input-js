0acd034b9374435a518372f3282e36fc120129de
Polyester <anonymous_571@outlook.com>
2020-08-05 18:02:05 +0300

    fix

--- input/input.js
+++ input/input.js
@@ -1,49 +1,5 @@
 const readline = require("readline");
-// const { resolve } = require("path");
-// const { rejects } = require("assert");
 
-// function input(keyvalue) {
-//   var lines = keyvalue["lines"];
-//   if (!lines) lines = 1;
-//   else if (lines === "inf") lines = -1;
-//   const prompt = keyvalue["prompt"];
-//   var data = "";
-//   if (prompt) process.stdin.write(prompt);
-
-//   process.stdin.resume();
-
-//   readline.emitKeypressEvents(process.stdin);
-//   process.stdin.setRawMode(true);
-
-//   process.stdin.on("keypress", function (chunk, key) {
-//     process.stdout.write(key.sequence);
-
-//     if (key && key.ctrl) {
-//       const ctrlkey = keyvalue["ctrl-" + key.name];
-//       if (ctrlkey) ctrlkey();
-//     } else if (key) {
-//       data += key.sequence;
-
-//       const tmpkey = keyvalue[key.name];
-//       //all keys on the keyboard
-//       if (tmpkey) tmpkey();
-//       //backspace if not specified
-//       else if (!tmpkey && key.name === "backspace") {
-//         process.stdin.write("\b \b");
-//         data = data.slice(0, -2);
-//       }
-//       //response if return not specified
-//       else if (!tmpkey && key.name === "return") {
-//         --lines;
-//         if (!lines) process.stdin.pause();
-//         process.stdin.write("\n");
-//         const reskey = keyvalue["response"];
-//         if (reskey) reskey(data);
-//         data = "";
-//       }
-//     }
-//   });
-// }
 process.stdin.pause();
 async function input(keyvalue) {
   return new Promise((resolve, reject) => {
